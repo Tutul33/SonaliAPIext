68da215fcabe496c877ed5316c87df66544cca40
Tutul Chakma <tutulcou@gmail.com>
2025-08-18 17:17:11 +0600

    ss

--- Sonali.API.Tests/UnitTests/Repository/VoucherRepositoryTests.cs
+++ Sonali.API.Tests/UnitTests/Repository/VoucherRepositoryTests.cs
@@ -1,13 +1,14 @@
-﻿using Xunit;
-using Moq;
-using AutoMapper;
+﻿using AutoMapper;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Sonali.API.Domain.DTOs;
 using Sonali.API.Infrustructure.DAL.Repository;
 using Sonali.API.Infrustructure.Data.Data;
-using Sonali.API.Domain.DTOs;
-using System.Threading.Tasks;
-using System.Collections.Generic;
 using Sonali.API.Infrustructure.Data.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
 namespace Sonali.API.Tests.UnitTests.Repository
 {
     public class VoucherRepositoryTests
@@ -18,19 +19,21 @@ namespace Sonali.API.Tests.UnitTests.Repository
 
         public VoucherRepositoryTests()
         {
-            // Setup in-memory DbContext
-            var options = new DbContextOptionsBuilder<AppDbContext>()
-                .UseInMemoryDatabase(databaseName: "TestDb")
-                .Options;
-
-            _dbContext = new AppDbContext(options);
+            // Setup isolated service provider for InMemory database
+            var services = new ServiceCollection();
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseInMemoryDatabase("TestDb");
+            });
+            var serviceProvider = services.BuildServiceProvider();
+            _dbContext = serviceProvider.GetRequiredService<AppDbContext>();
 
             // Setup AutoMapper
-            var mockMapper = new MapperConfiguration(cfg =>
+            var config = new MapperConfiguration(cfg =>
             {
                 cfg.CreateMap<VoucherUpdateDTO, Accgl2025>().ReverseMap();
             });
-            _mapper = mockMapper.CreateMapper();
+            _mapper = config.CreateMapper();
 
             _repository = new VoucherRepository(_dbContext, _mapper);
         }
@@ -38,25 +41,23 @@ namespace Sonali.API.Tests.UnitTests.Repository
         [Fact]
         public async Task UpdateVoucher_ShouldAddNewVoucher()
         {
-            // Arrange
             var voucherList = new List<VoucherUpdateDTO>
-        {
-            new VoucherUpdateDTO
             {
-                Id = 0,
-                Tag = EntityState.Added,
-                Damount = 100,
-                Camount = 100
-            }
-        };
-
-            // Act
+                new VoucherUpdateDTO
+                {
+                    Id = 0,
+                    Tag = EntityState.Added,
+                    Damount = 100,
+                    Camount = 100,
+                    UpdateType = "referral-pending" // important
+                }
+            };
             var result = await _repository.UpdateVoucher(voucherList);
 
-            // Assert
             Assert.Single(result);
             Assert.Single(_dbContext.Accgl2025s);
             Assert.Equal(100, _dbContext.Accgl2025s.First().Damount);
         }
     }
+
 }
